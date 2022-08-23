cbb0fc657882a418bb5921b69d6235ecad1f0274
Raksita <raksita.pu.63@ubu.ac.th>
2022-08-23 15:35:23 +0700

    11111

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
@@ -9,22 +10,45 @@ public class Player : MonoBehaviour
     AudioSource asource;
     float hp = 100;
     Image imgHP;
+    public GameObject text1, button1;
+    Zombie myZombie;
     
-    // Start is called before the first frame update
     void Start()
     {
         asource = GetComponent<AudioSource>();
         imgHP =  GameObject.Find("HP").GetComponent<Image>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             asource.PlayOneShot(firearm);
+            Ray ray;
+            RaycastHit hit;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.CompareTag("Zombie"))
+                {
+                    myZombie = hit.collider.gameObject.GetComponent<Zombie>();
+                    myZombie.zombieHP -= 25;
+                    if (myZombie.zombieHP <= 0)
+                    {
+                        NavMeshAgent agent = myZombie.GetComponent<NavMeshAgent>();
+                        Animator anim = myZombie.GetComponent<Animator>();
+                        agent.speed = 0;
+                        anim.SetFloat("hp", 0);
+                    }
+                }
+            }
         }
         imgHP.fillAmount = hp / 100;
+        if (hp <= 0)
+        {
+            text1.SetActive(true);
+            button1.SetActive(true);
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -39,7 +63,7 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Zombie"))
         {
-            hp -= 0.1f;
+            hp -= 0.3f;
         }
     }
 }
